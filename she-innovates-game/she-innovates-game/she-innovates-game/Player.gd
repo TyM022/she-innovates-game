99b833a8ef1983f0cfa440c8955ff48e85bae7f1
Tyler Miller <tym.22@outlook.com>
2026-02-07 11:23:13 -0500

    uhhhhhh

---
+++ she-innovates-game/she-innovates-game/she-innovates-game/Player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+@export var speed : int = 100
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+func _process(delta: float) -> void: 
+	get_input()
+	update_animation()
+	move_and_slide()
+	
+
+func get_input() -> void:
+	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity  = input_direction * speed
+	
+func update_animation() -> void:
+	if velocity.x < 0 || velocity.y < 0 && velocity.x <= 0:
+		animation_player.play("WalkLeft")
+	elif velocity.x > 0 || velocity.y > 0 && velocity.x >= 0:
+		animation_player.play("WalkRight")
+	else:
+		animation_player.play("Idle")
